8a0c3f680779c47f13c91aa7569fbfba70b5b113
Josep Ll. Gelpi <gelpi@ub.edu>
2022-02-14 20:03:18 +0100

    Moved to vuetify

--- pages/show.vue
+++ pages/show.vue
@@ -1,64 +1,87 @@
 <template>
-<div class="container">
-    <h1>{{ title }} - {{ pdbData.idCode}}</h1>
-    <p v-if="$fetchState.pending">Waiting data...</p>
-    <p v-else-if="error.message">{{error.message}}</p>
-    <table v-else class="table table-hover">
-    <tbody>
-        <tr>
-            <td>PDB reference</td>
-            <td>{{ pdbData.idCode }}</td>
-            <td rowspan="5">
+<v-app>
+    <v-main>
+        <v-container>
+            <v-sheet
+                color="white"
+                elevation="3"
+                outlined
+                width="900"
+            >
+                <v-card>
+                    <v-container>
+                    <h1>{{ title }} - {{ this.$route.query.id}}</h1>
+                    <p v-if="$fetchState.pending">Waiting data...</p>
+                    <p v-else-if="error.message">{{error.message}}</p>
+                    </v-container>
+                </v-card>
+                <v-card v-if="pdbData.idCode">
+                    <v-container>
+                        <v-row>
+                            <v-col sm="8">
+                                <v-simple-table>
+                                    <tbody>
+                                        <tr>
+                                            <td>PDB reference</td> <td>{{ pdbData.idCode }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Header</td>
+                                            <td>{{ pdbData.header }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Title</td>
+                                            <td>{{ pdbData.compound }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Resolution</td>
+                                            <td>
+                                                <span v-if="pdbData.resolution">{{ pdbData.resolution }}</span>
+                                                <span v-else>N.D.</span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Ascession Date</td>
+                                            <td>{{ pdbData.ascessionDate }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Experiment type</td>
+                                            <td>{{ pdbData.ExpType }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Authors</td>
+                                            <td>
+                                                <span v-for="aut in pdbData.auts">{{ aut }}&nbsp;</span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Source</td>
+                                            <td>
+                                                <span v-for="s in pdbData.sources">{{s}}</span>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </v-simple-table>
+                            </v-col>
+                            <v-col sm="4">
                 <a :href="`${this.$config.PDBLink}${pdbData.idCode.toUpperCase()}`" target="_blank">
                     <img :src="`${this.$config.ImgLink}${pdbData.idCode.toLowerCase()}.png`" border="0" width="250" ><br>
                         Link to Protein Data Bank</a>
-            </td>
-        </tr>
-        <tr>
-            <td>Header</td>
-            <td>{{ pdbData.header }}</td>
-        </tr>
-        <tr>
-            <td>Title</td>
-            <td>{{ pdbData.compound }}</td>
-        </tr>
-        <tr>
-            <td>Resolution</td>
-            <td v-if="pdbData.resolution">{{ pdbData.resolution }}</td>
-            <td v-else>N.D.</td>
-        </tr>
-        <tr>
-            <td>Ascession Date</td>
-            <td>{{ pdbData.ascessionDate }}</td>
-        </tr>
-        <tr>
-            <!--$expTypeArray is generated in globals.inc.php-->
-            <td>Experiment type</td> 
-            <td colspan="2">{{ pdbData.ExpType }}</td>
-        </tr>
-        <tr>
-            <td>Authors</td>
-            <td colspan="2">
-                <span v-for="aut in pdbData.auts">{{ aut }}&nbsp;</span>
-            </td>
-        </tr>
-        <tr>
-            <td>Source</td>
-            <td colspan="2">
-                <span v-for="s in pdbData.sources">{{s}}</span>
-                
-                </td>
-        </tr>
-        <tr>
-            <td>Sequence(s)</td>
-            <td colspan="2">
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col sm="1"/>
+                            <v-col sm="7">
+                        <h6>Sequence(s)</h6>
                 <p v-for="sq in pdbData.sequencesFASTA">
                     <pre> {{ sq }} </pre></p>
-           </td>
-        </tr>
-    </tbody>
-</table>
-</div>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card>
+            </v-sheet>
+        </v-container>
+    </v-main>
+</v-app>
 </template>
 
 <script>
