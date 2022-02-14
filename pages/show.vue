90259273b9e1683d36781ea7e469997c866459f9
Josep Ll. Gelpi <gelpi@ub.edu>
2022-02-14 20:32:39 +0100

    Improved layout

--- pages/show.vue
+++ pages/show.vue
@@ -26,11 +26,11 @@
                                         </tr>
                                         <tr>
                                             <td>Header</td>
-                                            <td>{{ pdbData.header }}</td>
+                                            <td class="text-capitalize">{{ pdbData.header.toLowerCase() }}</td>
                                         </tr>
                                         <tr>
                                             <td>Title</td>
-                                            <td>{{ pdbData.compound }}</td>
+                                            <td class="text-capitalize">{{ pdbData.compound.toLowerCase() }}</td>
                                         </tr>
                                         <tr>
                                             <td>Resolution</td>
@@ -49,36 +49,39 @@
                                         </tr>
                                         <tr>
                                             <td>Authors</td>
-                                            <td>
-                                                <span v-for="aut in pdbData.auts">{{ aut }}&nbsp;</span>
+                                            <td class="text-capitalize">
+                                                {{ pdbData.auts.join(', ').toLowerCase()}}
                                             </td>
                                         </tr>
                                         <tr>
                                             <td>Source</td>
-                                            <td>
-                                                <span v-for="s in pdbData.sources">{{s}}</span>
+                                            <td class="text-capitalize">
+                                                {{ pdbData.sources.join(', ').toLowerCase() }}
                                             </td>
                                         </tr>
                                     </tbody>
                                 </v-simple-table>
                             </v-col>
                             <v-col sm="4">
-                <a :href="`${this.$config.PDBLink}${pdbData.idCode.toUpperCase()}`" target="_blank">
-                    <img :src="`${this.$config.ImgLink}${pdbData.idCode.toLowerCase()}.png`" border="0" width="250" ><br>
-                        Link to Protein Data Bank</a>
+                                <v-card width="250" elevation="0">
+                                    <v-img :src="`${this.$config.ImgLink}${pdbData.idCode.toLowerCase()}.png`"/>
+                                    <v-card-text>
+                                        <a :href="`${this.$config.PDBLink}${pdbData.idCode.toUpperCase()}`" target="_blank">Browse data in Protein Data Bank</a>
+                                    </v-card-text>
+                                </v-card>
                             </v-col>
                         </v-row>
                         <v-row>
-                            <v-col sm="1"/>
-                            <v-col sm="7">
-                        <h6>Sequence(s)</h6>
-                <p v-for="sq in pdbData.sequencesFASTA">
-                    <pre> {{ sq }} </pre></p>
+                            <v-col sm="8">
+                                <h6>Sequence(s)</h6>
+                                <p v-for="sq in pdbData.sequencesFASTA">
+                                <pre> {{ sq }} </pre></p>
                             </v-col>
                         </v-row>
                     </v-container>
                 </v-card>
             </v-sheet>
+            <v-btn @click="newSearch()">New Search</v-btn>
         </v-container>
     </v-main>
 </v-app>
@@ -95,16 +98,21 @@ export default {
         }
     },
     async fetch() {
-            const dataResponse = await fetch(
-                `${this.$config.APIPrefix}?show=${this.$route.query.id}`
-            )
-            if (dataResponse.ok) {
-                this.pdbData = await dataResponse.json();
-                if (!this.pdbData.idCode) {
-                    this.error = this.pdbData
-                }
-            }
-        },
+        const dataResponse = await fetch(
+            `${this.$config.APIPrefix}?show=${this.$route.query.id}`
+        )
+        if (dataResponse.ok) {
+            this.pdbData = await dataResponse.json();
+            if (!this.pdbData.idCode) {
+                this.error = this.pdbData
+            } 
+        }
+    },
+    methods: {
+        newSearch() {
+            this.$router.push("/");
+        }
+    },
     fetchOnServer: false
 }
 
